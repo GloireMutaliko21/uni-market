7f9b7fb26be20844deb303133df5ef7e409a3f72
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-09-11 11:45:51 +0200

    premier commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center h-screen text-9xl font-black text-sky-800'>
+      Hello world !
+    </div>
+  )
+}
+
+export default App
