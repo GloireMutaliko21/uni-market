cdff8b9b0923737f681626353055c8ba66ca8d88
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-09-12 17:22:43 +0200

    feat: adding page loader

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const WelcomePage = lazy(() => import('./components/WelcomePage'));
 // import WelcomePage from './components/WelcomePage'
 import { useStateContext } from "./context/ContextProvider";
 import Login from './components/Login';
+import PageLoader from "./components/PageLoader";
 
 function App() {
   const { start } = useStateContext();
@@ -13,14 +14,14 @@ function App() {
     <div className='mx-24 my-1 flex items-center justify-center content-center'>
       {
         !start &&
-        <Suspense fallback='Loading'>
+        <Suspense fallback={<PageLoader />}>
           <WelcomePage />
         </Suspense>
 
       }
       {
         start &&
-        <Suspense fallback='Loading'>
+        <Suspense fallback={<PageLoader />}>
           <Login />
         </Suspense>
       }
---
+++ src/components/PageLoader.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const PageLoader = () => {
+    return (
+        <div className="grid place-items-center bg-gradient-to-l from-indigo-100 to-indigo-900 h-screen w-screen">
+            <div className="w-full h-full bg-white rounded-md shadow-xl m-3">
+                <div className="h-44 bg-gray-300 rounded-t-md animate-pulse"></div>
+                <div className="p-5">
+                    <div className="h-6 rounded-sm bg-gray-200 duration-75 animate-pulse mb-4"></div>
+                    <div className="animate-pulse">
+                        <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
+                            <div className="border-t-black animate-spin ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+
+                        </div>
+                        <div className="h-3 mt-2 w-1/2 rounded-sm bg-gray-300"></div>
+                        <div className="h-5 mt-2 w-1/3 rounded-sm bg-gray-300"></div>
+                        <div className="h-5 mt-2 w-2/3 rounded-sm bg-gray-300"></div>
+                        <div className="h-2 mt-2 rounded-sm bg-gray-300"></div>
+                        <div className="h-4 mt-2 rounded-sm bg-gray-300"></div>
+                        <div className="h-4 mt-2 rounded-sm bg-gray-300"></div>
+                        <div className="h-3 rounded-sm bg-gray-300"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PageLoader
