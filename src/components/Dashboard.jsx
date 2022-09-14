84113ad01cabd1cd9f5837dae00fe1481174cd1a
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-09-14 22:52:46 +0200

    feat: alterner entre composants login et dashboard

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import Button from './Button';
+import { useStateContext } from "../context/ContextProvider";
+
+const Dashboard = () => {
+    const { setUserData, setLoginStatus } = useStateContext();
+    const handleLogout = () => {
+        localStorage.removeItem('isLogged');
+        setUserData({});
+        setLoginStatus(false);
+    }
+
+    return (
+        <div className='text-9xl font-bold text-center'>
+            Dashboard
+            <Button
+                label='Deconnexion'
+                style='flex justify-center w-full bg-blue-gray-800 hover:bg-blue-gray-700 text-white font-semibold p-3'
+                onClick={handleLogout}
+            />
+        </div>
+    )
+}
+
+export default Dashboard
