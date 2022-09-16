d9c3b4bff3258fdb6a5b2acf9570802768750d83
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-09-16 21:31:15 +0200

    init donnees routes

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,102 @@
+export const mainMenus = [
+    "Home",
+    "Opérations",
+    "Comptabilité",
+    "Visualisation",
+    "Partenaires",
+    "Configuration",
+]
+
+export const routesData = [
+    {
+        title: 'Dashoboard',
+        path: '/',
+        element: '',
+        parent: mainMenus[0]
+    },
+    {
+        title: 'Approvisionnement',
+        path: '/approv',
+        element: '',
+        parent: mainMenus[1]
+    },
+    {
+        title: 'Ventes',
+        path: '/vente',
+        element: '',
+        parent: mainMenus[1]
+    },
+    {
+        title: 'Paiements',
+        path: '/paie',
+        element: '',
+        parent: mainMenus[1]
+    },
+    {
+        title: 'Caisse',
+        path: '/caisse',
+        element: '',
+        parent: mainMenus[1]
+    },
+    {
+        title: 'Produits',
+        path: '/produit',
+        element: '',
+        parent: mainMenus[2]
+    },
+    {
+        title: 'Catégories',
+        path: '/categ',
+        element: '',
+        parent: mainMenus[2]
+    },
+    {
+        title: 'Comptes',
+        path: '/comptes',
+        element: '',
+        parent: mainMenus[2]
+    },
+    {
+        title: 'Types comptes',
+        path: '/type-cpte',
+        element: '',
+        parent: mainMenus[2]
+    },
+    {
+        title: 'Stocks',
+        path: '/stocks',
+        element: '',
+        parent: mainMenus[3]
+    },
+    {
+        title: 'Factures',
+        path: '/factures',
+        element: '',
+        parent: mainMenus[3]
+    },
+    {
+        title: 'Fournisseurs',
+        path: '/fournisseurs',
+        element: '',
+        parent: mainMenus[4]
+    },
+    {
+        title: 'Clients',
+        path: '/clients',
+        element: '',
+        parent: mainMenus[4]
+    },
+    {
+        title: 'Utilisateurs',
+        path: '/clients',
+        element: '',
+        parent: mainMenus[5]
+    },
+    {
+        title: 'Taux de change',
+        path: '/taux',
+        element: '',
+        parent: mainMenus[5]
+    },
+
+]
