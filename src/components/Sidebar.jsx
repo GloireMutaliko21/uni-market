1120025c5819a4c379e3275aa4496d371c515b7b
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-09-16 23:12:44 +0200

    style: ajout des accordions sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,22 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from "react-router-dom";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import {
+    Accordion,
+    AccordionHeader,
+    AccordionBody,
+} from "@material-tailwind/react";
 
 import { mainMenus, routesData } from "../routes/Routes";
 
 const Sidebar = () => {
+    const [open, setOpen] = useState(0);
+
+    const handleOpen = (value) => {
+        setOpen(open === value ? 0 : value);
+    };
+
+    const customAnimation = {
+        mount: { scale: 1 },
+        unmount: { scale: 0.9 },
+    };
+
     return (
-        <div className='bock'>
+        <div className='bock absolute left-0 top-5 mx-5 text-red-800'>
             {mainMenus.map((menu, index) =>
-                <div key={index}>
-                    <p className='uppercase'>{menu}</p>
-                    <div className='grid ml-5'>
+                <Accordion
+                    open={open === index + 1}
+                    key={index}
+                    icon={<MdOutlineKeyboardArrowDown className={`${open === index + 1 ? "rotate-180" : ""}`} />}
+                    animate={customAnimation}
+                    className=''
+                >
+                    <AccordionHeader onClick={() => handleOpen(index + 1)} className='text-xs uppercase'>
+                        {menu}
+                    </AccordionHeader>
+                    <AccordionBody className='grid ml-5'>
                         {routesData.map((route, idx) =>
                             route.parent === menu &&
                             <NavLink to={route.path} key={idx}>{route.title}</NavLink>
 
                         )}
-                    </div>
-                </div>
+                    </AccordionBody>
+                </Accordion>
             )}
         </div>
     )
