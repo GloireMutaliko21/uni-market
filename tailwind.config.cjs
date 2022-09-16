ed8054ed7a7f43c795b2dc83ce6a274067be62c0
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-09-16 19:25:43 +0200

    style: couleurs personnalisees

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,17 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'titleColor': '#0A1E2E',
+        'text': '#00020B',
+        'body': '#BEEBFC',
+        'button': '#BEEBFC',
+        'subComponent': '#515F6A',
+        'default': '#515F6A',
+        'blue': '#1CB2F6'
+      }
+    },
   },
   plugins: [],
 });
