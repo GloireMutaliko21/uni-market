455e45f6233f1ab2e3a9ef3ae73de1678ba36996
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-09-16 20:38:26 +0200

    refact: style formulaire connexion

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -14,7 +14,7 @@ const Input = ({ label, value, onChange, type, name, icon, iconMask }) => {
             <label className='font-bold text-sm'>{label}</label>
             <div className='bg-gray-200 flex justify-between items-center relative mt-2 mb-4'>
                 <input
-                    className="w-full bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block appearance-none"
+                    className="w-full text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block appearance-none"
                     type={type}
                     value={value}
                     onChange={onChange}
@@ -22,7 +22,7 @@ const Input = ({ label, value, onChange, type, name, icon, iconMask }) => {
                 >
                 </input>
                 <span
-                    className='absolute right-3 hover:cursor-pointer text-lg text-light-blue-900'
+                    className='absolute right-3 hover:cursor-pointer text-lg text-teal-900'
                     onClick={handleChangeIcon}
                 >
                     {!showPassword ? icon : iconMask}
