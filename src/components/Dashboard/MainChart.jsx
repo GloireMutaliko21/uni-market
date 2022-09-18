1827cd04067ada5ed01aa75ab4c698e215b30495
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-09-18 23:44:01 +0200

    style: main chart

--- src/components/Dashboard/MainChart.jsx
+++ src/components/Dashboard/MainChart.jsx
@@ -1,37 +1,36 @@
 import React from 'react'
-import { AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area, ResponsiveContainer, Legend } from "recharts";
+import { AreaChart, ComposedChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Area, ResponsiveContainer, Legend } from "recharts";
 
 const MainChart = () => {
     const data = [
         {
             name: 'Jan',
             uv: 4000,
-            pv: 2400,
+            pv: 1500,
             amt: 2400,
         },
         {
             name: 'Fev',
-            uv: 3000,
-            pv: 1398,
+            uv: 1500,
+            pv: 4398,
             amt: 2210,
         },
         {
             name: 'Mar',
-            uv: 2000,
-            pv: 9800,
+            uv: 2500,
+            pv: 1000,
             amt: 2290,
         },
         {
             name: 'Avr',
-            uv: 2780,
+            uv: 1700,
             pv: 3908,
             amt: 2000,
         },
         {
             name: 'Mai',
-            uv: 1890,
-            pv: 4800,
-            amt: 2181,
+            uv: 4890,
+            pv: 1800,
         },
         {
             name: 'Jun',
@@ -50,7 +49,7 @@ const MainChart = () => {
     return (
         <div className='w-full h-64 border my-5 p-5 rounded-2xl text-xs'>
             <ResponsiveContainer width="100%" height="100%">
-                <AreaChart
+                <ComposedChart
                     width={500}
                     height={400}
                     data={data}
@@ -61,13 +60,26 @@ const MainChart = () => {
                         bottom: 0,
                     }}
                 >
+                    <defs>
+                        <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0} />
+                            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                        </linearGradient>
+                        <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#3a466a" stopOpacity={0.4} />
+                            <stop offset="95%" stopColor="#3a466a" stopOpacity={0} />
+                        </linearGradient>
+                    </defs>
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="name" />
                     <YAxis />
                     <Tooltip />
-                    <Area type="monotone" dataKey="uv" stackId="1" stroke="#f1f1f6" fill="#f1f1f6" />
-                    <Area type="monotone" dataKey="pv" stackId="1" stroke="#f1f1f6" fill="#82ca9d" />
-                </AreaChart>
+                    {/* <Line type="monotone" dataKey="pv" stroke="#ff7300" /> */}
+                    <Area type="monotone" dataKey="pv" stackId="1" stroke="#3a466a" fillOpacity={1} fill="url(#colorPv)" />
+                    <Line type="monotone" dataKey="uv" stroke="#6CBAB1" />
+                    {/* <Area type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" /> */}
+
+                </ComposedChart>
             </ResponsiveContainer>
         </div>
     )
