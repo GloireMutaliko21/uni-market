82675283e1a58ad91dc9ca6564ff825c1882926e
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-09-24 18:38:00 +0200

    style: No surch data sur recherche

--- src/components/Approvisionnement/Produits.jsx
+++ src/components/Approvisionnement/Produits.jsx
@@ -1,9 +1,9 @@
 import React, { useRef } from 'react'
-import { MdAddCircle } from "react-icons/md";
+import { MdAddCircle, MdOutlineWarning } from "react-icons/md";
+import { FcHighPriority } from "react-icons/fc";
 
 import { getProduits } from '../../constants/api'
 import { useStateContext } from "../../context/ContextProvider";
-import Input from '../Input';
 
 const Produits = () => {
     const searchRef = useRef();
@@ -27,21 +27,24 @@ const Produits = () => {
     getProduits();
     return (
         <div>
-            <Input
-                reference={searchRef}
+            <input
+                ref={searchRef}
+                className="w-full text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-1 px-4 my-2 block appearance-none"
                 type='text'
                 name=''
+                placeholder='Rechercher'
                 value={isFilter}
                 onChange={(e) => { handleChangeIsFilter(e) }}
-            />
-            {productsData.map((prod, index) => (
+            >
+            </input>
+            {productsData.length > 0 ? productsData.map((prod, index) => (
                 <div key={index} className={`${index % 2 === 0 ? 'bg-teal-200' : 'border border-teal-100 text-teal-300 font-medium'} flex justify-between py-1 px-2 text-titleColor hover:cursor-pointer`}>
                     <p className='uppercase'>{prod.designation}</p>
                     <p>
                         <MdAddCircle className='text-lg ml-5' />
                     </p>
                 </div>
-            ))}
+            )) : <div className='text-teal-600 font-light flex items-center justify-center'><FcHighPriority className='mr-4 text-lg' />Aucun produit</div>}
         </div>
     )
 }
