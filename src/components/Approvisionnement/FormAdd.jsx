c326e4f6aca0fd1776dc4a134de40f90a1625c0d
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-10-02 21:38:34 +0200

    remove comments

--- src/components/Approvisionnement/FormAdd.jsx
+++ src/components/Approvisionnement/FormAdd.jsx
@@ -146,9 +146,6 @@ const FormAdd = () => {
                             </div>
                         </div>
                         <div className='flex flex-col justify-between'>
-                            {/* <div className='text-end'> */}
-                            {/* <p className='text-sm text-green-800'>Tot: <span className='font-bold text-base'>{parseFloat(produit.qte) * parseFloat(produit.pu)}</span></p> */}
-                            {/* </div> */}
                             <div className='flex flex-col text-sm gap-2'>
                                 {index !== productIndex &&
                                     <div className='text-white text-lg bg-teal-800 py-1 px-2 rounded shadow-lg shadow-gray-500'>
